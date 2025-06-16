9c8f90fd43b3f56d9481debb4ba8d36bc4f16abf
iamlee103 <iamlee103@gmail.com>
2025-06-16 13:45:58 +0900

    'DB_connecting'

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,23 @@
-// import postgres from 'postgres';
+import { prismaclient } from "@prisma/client";
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
-
-// 	return data;
-// }
+const prisma = new prismaclient();
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
-}
+  try {
+    const data = await prisma.$queryRaw<{
+      amount: number;
+      name: string;
+    }[]>`
+      SELECT Invoice.amount, Customer.name
+        FROM Invoice
+        JOIN Customer ON Invoice.customer_id = Customer.id
+        WHERE Invoice.amount = 666;
+      `;
+  
+      return Response.json(data);
+    } catch (error) {
+      return Response.json({ error }, { status: 500 });
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
