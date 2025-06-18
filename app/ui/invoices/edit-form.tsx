189332985207cc1a5a15a3bbc4d3108c7d17d5de
iamlee103 <iamlee103@gmail.com>
2025-06-19 02:36:01 +0900

    for present

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -11,14 +11,16 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
+import type { State } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({invoice, customers}: {
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const initialState: State = { message: null, errors: {} };    
-  const [state, formAction] = useActionState(updateInvoice, initialState);
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);  
+  const initialState: State = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+
 
   return <form action={formAction}>
     <div className="rounded-md bg-gray-50 p-4 md:p-6">
